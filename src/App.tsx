6a73f3e11dfe27e1a1c45b976766273497c212ec
ductaip <dtai858@gmail.com>
2025-02-05 22:25:28 +0700

    first init for project frontend with redux toolkit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+ 
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 className="text-3xl text-pink-500 font-bold underline">
+          Hello world!
+        </h1>
+    </>
+  )
+}
+
+export default App
