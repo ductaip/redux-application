59591039834e197c71153ad235c3eb7617b5e379
ductaip <dtai858@gmail.com>
2025-02-06 20:44:06 +0700

    integrate data list and add into blog

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,35 +1,28 @@
-import React from 'react'
-
-export default function PostItem() {
-  return (
-    <form>
-        <div className="mb-6">
-            <label htmlFor="title" className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300">Title</label><input type="text" id="title" className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500" placeholder="Title" required />
-        </div>
-        <div className="mb-6">
-            <label htmlFor="featuredImage" className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300">Featured Image</label><input type="text" id="featuredImage" className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500" placeholder="Url image" required  />
-        </div>
-        <div className="mb-6">
-            <div>
-            <label htmlFor="description" className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-400">Description</label><textarea id="description" rows={3} className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500" placeholder="Your description..." required/>
-            </div>
-        </div>
-        <div className="mb-6">
-            <label htmlFor="publishDate" className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300">Publish Date</label><input type="datetime-local" id="publishDate" className="block w-56 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500" placeholder="Title" required  />
-        </div>
-        <div className="mb-6 flex items-center">
-            <input id="publish" type="checkbox" className="h-4 w-4 focus:ring-2 focus:ring-blue-500" /><label htmlFor="publish" className="ml-2 text-sm font-medium text-gray-900">Publish</label>
-        </div>
-        <div>
-            <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800" type="submit">
-            <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">Publish Post</span>
-            </button>
-            <button type="submit" className="group relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-teal-300 to-lime-300 p-0.5 text-sm font-medium text-gray-900 focus:outline-none focus:ring-4 focus:ring-lime-200 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 dark:focus:ring-lime-800">
-            <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">Update Post</span></button><button type="reset" className="group relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 p-0.5 text-sm font-medium text-gray-900 focus:outline-none focus:ring-4 focus:ring-red-100 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 dark:focus:ring-red-400">
-            <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">Cancel</span>
-            </button>
-        </div>
-    </form>
-
-  )
+  export default function PostItem() {
+    return (
+      <div className="flex flex-col items-center overflow-hidden rounded-lg border md:flex-row">
+                <div className="group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48">
+                  <img src="https://images.unsplash.com/photo-1665412019489-1928d5afa5cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80" loading="lazy" alt="Mọi công việc thành đạt đều nhờ sự kiên trì và lòng say mê." className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+                </div>
+                <div className="flex flex-col gap-2 p-4 lg:p-6">
+                  <span className="text-sm text-gray-400">2022-10-13T11:32</span>
+                  <h2 className="text-xl font-bold text-gray-800">
+                    Mọi công việc thành đạt đều nhờ sự kiên trì và lòng say mê.
+                  </h2>
+                  <p className="text-gray-500">
+                    Nghịch cảnh là một phần của cuộc sống. Nó không thể bị kiểm
+                    soát. Cái có thể kiểm soát chính là cách chúng ta phản ứng
+                    trước nghịch cảnh.
+                  </p>
+                  <div>
+                    <div className="inline-flex rounded-md shadow-sm" role="group">
+                      <button type="button" className="rounded-l-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700">
+                        Edit</button><button type="button" className="rounded-r-lg border-t border-b border-r border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700">
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    )
 }
