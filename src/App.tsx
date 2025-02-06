46a7aedad9e8f06fb7f4a59aa8b6263710e4380e
ductaip <dtai858@gmail.com>
2025-02-06 21:03:14 +0700

    add all component and adjust structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
  
 import './index.css'
+import Blog from './pages/blog'
 
 function App() {
 
-  return (
-    <>
-        <h1 className="text-3xl text-pink-500 font-bold underline">
-          Hello world!
-        </h1>
-    </>
-  )
+  return <Blog />
 }
 
 export default App
--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import CreatePost from '@/components/CreatePost'
+import PostList from '@/components/PostList'
 
 export default function Blog() {
   return (
     <div className='p-5'>
-      blog
+      <CreatePost />
+      <PostList />
     </div>
   )
 }
